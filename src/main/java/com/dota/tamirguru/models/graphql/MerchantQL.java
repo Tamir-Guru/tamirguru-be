d342c2b3f22e673a4a52a5a2bbbcf6b3bcb5a5a5
ogzkhrmn <ogz.khrmn52@gmail.com>
2022-01-01 14:37:15 +0300

    Fix config errors, add filter, add graphql support

---
+++ src/main/java/com/dota/tamirguru/models/graphql/MerchantQL.java
@@ -0,0 +1,13 @@
+/*
+ * @author : Oguz Kahraman
+ * @since : 31 Ara 2021
+ *
+ * Copyright - TamirGuru
+ */
+package com.dota.tamirguru.models.graphql;
+
+import lombok.Data;
+
+@Data
+public class MerchantQL {
+}
